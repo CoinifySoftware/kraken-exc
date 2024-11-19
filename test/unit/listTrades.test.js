d1d7f810b7d597800661a931a76da65dd2f41f81
hareland <packagist@proton.me>
2024-11-19 18:17:11 +0100

    CY-1799: slim down test

--- test/unit/listTrades.test.js
+++ test/unit/listTrades.test.js
@@ -15,48 +15,8 @@ describe('#listTrades', function () {
   beforeEach(() => {
     expectedTrades = [
       {
-        baseCurrency: 'ETH',
-        quoteCurrency: 'USD'
-      },
-      {
-        baseCurrency: 'EUR',
-        quoteCurrency: 'USD'
-      },
-      {
         baseCurrency: 'BTC',
         quoteCurrency: 'USD'
-      },
-      {
-        baseCurrency: 'BTC',
-        quoteCurrency: 'USD'
-      },
-      {
-        baseCurrency: 'BTC',
-        quoteCurrency: 'USD'
-      },
-      {
-        baseCurrency: 'ETH',
-        quoteCurrency: 'EUR'
-      },
-      {
-        baseCurrency: 'USDC',
-        quoteCurrency: 'EUR'
-      },
-      {
-        baseCurrency: 'USDC',
-        quoteCurrency: 'EUR'
-      },
-      {
-        baseCurrency: 'EUR',
-        quoteCurrency: 'USDC'
-      },
-      {
-        baseCurrency: 'EUR',
-        quoteCurrency: 'TRX'
-      },
-      {
-        baseCurrency: 'EUR',
-        quoteCurrency: 'ALGO'
       },
       {
         baseCurrency: 'ETH',
@@ -69,106 +29,10 @@ describe('#listTrades', function () {
     ];
 
     trades = {
-      '0000-0000-0000-0000': {
-        ordertxid: 'OQCLML-BW3P3-BUCMWZ',
-        postxid: 'TKH2SE-M7IF5-CFI7LT',
-        pair: 'XETHZUSD',
-        time: 0,
-        type: 'buy',
-        ordertype: 'limit',
-        price: '30010.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
-      'TCWJEG-FL4SZ-3FKGH6': {
-        ordertxid: 'OQCLML-BW3P3-BUCMWZ',
-        postxid: 'TKH2SE-M7IF5-CFI7LT',
-        pair: 'XETHZUSD',
-        time: Date.now() / 1000,
-        type: 'buy',
-        ordertype: 'limit',
-        price: '30010.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
-      'TAWJEG-FL4SZ-3FKGH6': {
-        ordertxid: 'OACLML-BW3P3-BUCMWZ',
-        postxid: 'TAH2SE-M7IF5-CFI7LT',
-        pair: 'ZEURZUSD',
-        time: Date.now() / 1000,
-        type: 'buy',
-        ordertype: 'limit',
-        price: '3001.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
-      'TBWJEG-FL4SZ-3FKGH6': {
-        ordertxid: 'OBCLML-BW3P3-BUCMWZ',
-        postxid: 'TBH2SE-M7IF5-CFI7LT',
-        pair: 'XETHZUSD',
-        time: 0,
-        type: 'buy',
-        ordertype: 'limit',
-        price: '3001.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
       'TYWJEG-FL4SZ-3FKGH6': {
         ordertxid: 'OAELML-BW3P3-BUCMWZ',
         postxid: 'TAH2SE-MEIF5-CFI7LT',
         pair: 'XBTZUSD',
-        time: Date.now() / 1000,
-        type: 'sell',
-        ordertype: 'limit',
-        price: '3001.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
-      'TXWJEG-FL4SZ-3FKGH6': {
-        ordertxid: 'OAELML-BW3P3-BUCMWZ',
-        postxid: 'TAH2SE-MEIF5-CFI7LT',
-        pair: 'XBTZUSD',
-        time: Date.now() / 1000,
-        type: 'sell',
-        ordertype: 'limit',
-        price: '3001.00000',
-        cost: '300.10000',
-        fee: '0.00000',
-        vol: '0.01000000',
-        margin: '0.00000',
-        misc: '',
-        trade_id: 39482674,
-        maker: true
-      },
-      'TXWJEG-FL4SZ-3FKYH6': {
-        ordertxid: 'OAELML-BW3P3-BUCMWZ',
-        postxid: 'TAH2SE-MEIF5-CFI7LT',
-        pair: 'XBTUSDC',
         time: Date.now() / 1000,
         type: 'sell',
         ordertype: 'limit',
@@ -192,78 +56,12 @@ describe('#listTrades', function () {
         price: '2333.92',
         status: 'closed'
       },
-      'OOEEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'USDCZEUR',
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
-      'OOUEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'USDCEUR', //Somehow, the kraken endpoint provides this asset pair when fetching trades.
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
-      'EOUEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'EURUSDC', //Somehow, the kraken endpoint provides this asset pair when fetching trades.
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
-      'IOUEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'ZEURTRX',
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
-      'ROUEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'ZEURALGO',
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
-      'KOUEE3QCQ-SOJXH-NWMZZF': {
-        fee: '104.68207',
-        vol: '24.91800000',
-        cost: '58156.69775',
-        time: Date.now() / 1000,
-        pair: 'ETHEUR',
-        type: 'buy',
-        ordertype: 'limit',
-        price: '2333.92',
-        status: 'closed'
-      },
       'KYUEE3QCQ-SOJXH-NWMZZF': {
         fee: '104.68207',
         vol: '24.91800000',
         cost: '58156.69775',
         time: Date.now() / 1000,
-        pair: 'EURETH',
+        pair: 'ZEURXETH',
         type: 'buy',
         ordertype: 'limit',
         price: '2333.92',
